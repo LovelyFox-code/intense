1d3344d6c718486e490675f2a9fbbb02d160868a
Alina <alinadakhno60@gmail.com>
2022-01-20 12:42:43 +0100

    add favicon

--- resourses/js/index.js
+++ resourses/js/index.js
@@ -1,56 +1,34 @@
+// slider carulel for video
+
 var slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+    showSlides(slideIndex += n);
 }
 
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+    showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("slides");
-  var dots = document.getElementsByClassName("demo");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+    var i;
+    var slides = document.getElementsByClassName("slides");
+    var dots = document.getElementsByClassName("demo");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
 
 }
 
 
-
-
-//counter
-
-const counter = document.getElementsByClassName('value');
-const speed = 2000;
-
-counter => {
-   const animate = () => {
-      const value = +counter.getAttribute('akhi');
-      const data = +counter.innerText;
-     
-      const time = value / speed;
-     if(data < value) {
-          counter.innerText = Math.ceil(data + time);
-          setTimeout(animate, 1);
-        }else{
-          counter.innerText = value;
-        }
-     
-   }
-   
-   animate();
-};
 
 //RANGE SLIDER
 
@@ -58,16 +36,16 @@ var range = document.getElementById("range");
 var output = document.getElementById("demo");
 output.innerHTML = range.value;
 
-range.oninput = function() {
-  output.innerHTML = this.value;
+range.oninput = function () {
+    output.innerHTML = this.value;
 }
 
 var range2 = document.getElementById("range2");
 var output2 = document.getElementById("number");
 output2.innerHTML = range2.value;
 
-range2.oninput = function() {
-  output2.innerHTML = this.value;
+range2.oninput = function () {
+    output2.innerHTML = this.value;
 }
 
 
@@ -77,40 +55,40 @@ range2.oninput = function() {
 const scrollElements = document.querySelectorAll(".js-scroll");
 
 const elementInView = (el, dividend = 1) => {
-  const elementTop = el.getBoundingClientRect().top;
+    const elementTop = el.getBoundingClientRect().top;
 
-  return (
-    elementTop <=
-    (window.innerHeight || document.documentElement.clientHeight) / dividend
-  );
+    return (
+        elementTop <=
+        (window.innerHeight || document.documentElement.clientHeight) / dividend
+    );
 };
 
 const elementOutofView = (el) => {
-  const elementTop = el.getBoundingClientRect().top;
+    const elementTop = el.getBoundingClientRect().top;
 
-  return (
-    elementTop > (window.innerHeight || document.documentElement.clientHeight)
-  );
+    return (
+        elementTop > (window.innerHeight || document.documentElement.clientHeight)
+    );
 };
 
 const displayScrollElement = (element) => {
-  element.classList.add("scrolled");
+    element.classList.add("scrolled");
 };
 
 const hideScrollElement = (element) => {
-  element.classList.remove("scrolled");
+    element.classList.remove("scrolled");
 };
 
 const handleScrollAnimation = () => {
-  scrollElements.forEach((el) => {
-    if (elementInView(el, 1.35)) {
-      displayScrollElement(el);
-    } else if (elementOutofView(el)) {
-      hideScrollElement(el)
-    }
-  })
+    scrollElements.forEach((el) => {
+        if (elementInView(el, 1.35)) {
+            displayScrollElement(el);
+        } else if (elementOutofView(el)) {
+            hideScrollElement(el)
+        }
+    })
 }
 
-window.addEventListener("scroll", () => { 
-  handleScrollAnimation();
+window.addEventListener("scroll", () => {
+    handleScrollAnimation();
 });
